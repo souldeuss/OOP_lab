f3ee81d94ea63dc52cd179220afd16a50db47e7f
souldeuss <skortkovvlad@gmail.com>
2023-09-12 20:52:50 +0300

    Fix bag

--- Lab_01_ch1/Program.cs
+++ Lab_01_ch1/Program.cs
@@ -24,34 +24,54 @@ namespace Lab_01_ch1
         {
             Console.WriteLine("Enter the name of group");
             string usrname = Console.ReadLine();
-            DateTime dateTime = new DateTime();
+            DateTime dateTime = DateTime.Now;
             Console.WriteLine(usrname + " " + dateTime);
         }
         static void Task2()
         {
+            Console.Write("Variant(1-9):");
+            int Var = Convert.ToInt32(Console.ReadLine());
+
             Console.Write("a:");
             double UsrVarA = Convert.ToDouble(Console.ReadLine());
             
             Console.Write("b:");
             double UsrVarB = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"1. {UsrVarA}^1/2 = {Math.Pow(UsrVarA, 0.5)}");
-
-            Console.WriteLine($"2. Exp({UsrVarA}) = {Math.Exp(UsrVarA)}");
-
-            Console.WriteLine($"3. Lg {UsrVarA} = {Math.Log10(UsrVarA)}");
-
-            Console.WriteLine($"4. Sin({UsrVarA}) = {(180 / Math.PI) * Math.Sin(UsrVarA)}°");
-
-            Console.WriteLine($"5. Cos({UsrVarA}) = {(180 / Math.PI) * Math.Cos(UsrVarA)}°");
-
-            Console.WriteLine($"6. Tg({UsrVarA}) = {(180 / Math.PI) * Math.Tan(UsrVarA)}°");
-
-            Console.WriteLine($"7. Ctg({UsrVarA}) = {(180 / Math.PI) / Math.Tan(UsrVarA)}°");
-
-            Console.WriteLine($"8. {UsrVarA}^{UsrVarB} = {Math.Pow(UsrVarA, UsrVarB)}");
-
-            Console.WriteLine($"9. ln({UsrVarA}) = {Math.Log(UsrVarA)}");
+            switch (Var)
+            {
+                case 1:
+                    Console.WriteLine($"1. {UsrVarA}^1/2 = {Math.Pow(UsrVarA, 0.5)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"2. Exp({UsrVarA}) = {Math.Exp(UsrVarA)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"3. Lg {UsrVarA} = {Math.Log10(UsrVarA)}");
+                    break;
+                case 4:
+                    Console.WriteLine($"4. Sin({UsrVarA}) = {(180 / Math.PI) * Math.Sin(UsrVarA)}°");
+                    break;
+                case 5:
+                    Console.WriteLine($"5. Cos({UsrVarA}) = {(180 / Math.PI) * Math.Cos(UsrVarA)}°");
+                    break;
+                case 6:
+                    Console.WriteLine($"6. Tg({UsrVarA}) = {(180 / Math.PI) * Math.Tan(UsrVarA)}°");
+                    break;
+                case 7:
+                    Console.WriteLine($"7. Ctg({UsrVarA}) = {(180 / Math.PI) / Math.Tan(UsrVarA)}°");
+                    break;
+                case 8:
+                    Console.WriteLine($"8. {UsrVarA}^{UsrVarB} = {Math.Pow(UsrVarA, UsrVarB)}");
+                    break;
+                case 9:
+                    Console.WriteLine($"9. ln({UsrVarA}) = {Math.Log(UsrVarA)}");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    Task2();
+                    break;
+            }
         }
         static void Task3()
         {
