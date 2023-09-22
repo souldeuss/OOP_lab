452b47c16206e6c7727a09f5d8445948eef3a994
souldeuss <107411064+souldeuss@users.noreply.github.com>
2023-09-22 23:50:35 +0300

    New lab

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace GoogleTranslateFreeApi.Examples
+{
+    class Program
+    {
+        private static readonly GoogleTranslator Translator = new GoogleTranslator();
+
+        static void Main(string[] args)
+        {
+            Language english = GoogleTranslator.GetLanguageByName("English"); // define language this way
+            Language russian = Language.Russian; // or even this way
+            Language french = GoogleTranslator.GetLanguageByISO("fr"); // you could also use ISO639 value
+
+            TranslationResult result = Translator.TranslateAsync("Hello. How are you?", english, russian).GetAwaiter().GetResult();
+
+            Console.WriteLine($"Result 1: {result.MergedTranslation}");
+
+            TranslationResult result2 =
+                Translator.TranslateAsync(new TranslateItem("The quick brown fox jumps over the lazy dog. Brown fox"))
+                    .GetAwaiter()
+                    .GetResult();
+
+            for (var i = 0; i < result2.FragmentedTranslation.Length; i++)
+            {
+                var part = result2.FragmentedTranslation[i];
+                Console.WriteLine($"Result 2.{i} = {part}");
+            }
+
+            TranslationResult result3 =
+                Translator.TranslateAsync("Run", english, Language.Dutch)
+                    .GetAwaiter()
+                    .GetResult();
+
+            foreach (var noun in result3.ExtraTranslations.Noun)
+            {
+                Console.WriteLine($"{noun.Phrase}: {string.Join(", ", noun.PhraseTranslations)}");
+            }
+        }
+
+        private struct TranslateItem : ITranslatable
+        {
+            public string OriginalText { get; }
+            public Language FromLanguage { get; }
+            public Language ToLanguage { get; }
+
+            // Some other user defined properties...
+
+            public TranslateItem(string text)
+            {
+                OriginalText = text;
+                FromLanguage = new Language("English", "en");
+                ToLanguage = GoogleTranslator.GetLanguageByISO("fr");
+            }
+        }
+    }
+}
