2c165594c7fb8fcb33db15f1048233d22143f7da
souldeuss <skortkovvlad@gmail.com>
2023-10-04 19:59:03 +0300

    Lab_02

---
+++ lab_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввід кількості рейсів
+            Print("Введiть кiлькiсть рейсiв: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            //Використання методу який запитує данні у користувача
+            Plane.UsrInput(n);
+
+            //Очищує консоль
+            Console.Clear();
+
+            //Виводить таблицю згідно кількості рейсів
+            Plane.TableOfPlane(n);
+
+            Console.ReadKey();
+        }
+        
+        //Метод замість Console.Write()
+        public static void Print(string word)
+        {
+            Console.Write(word);
+        }
+    }
+}
