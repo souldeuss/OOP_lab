d310cf9ee2f70bfdebef27e74a51dfa0d3935895
souldeuss <skortkovvlad@gmail.com>
2023-12-16 23:52:34 +0200

    Update Shape.cs

---
+++ lab_05/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_05
+{
+    internal interface Shape
+    {
+
+        void CalculateArea(int a, int b,object shape);
+    }
+}
